498894ead1c09c3dfcb0ab3c2a519a2f6216f078
fvillena <villenafabian@gmail.com>
2019-03-05 13:20:28 -0300

    new page title

--- resources/views/med.blade.php
+++ resources/views/med.blade.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>{{$med['name']}} ¿Con receta?</title>
+    <title>¿Con receta? {{$med['name']}}</title>
     <meta name="description" content="En esta página puedes revisar si el medicamento {{$med['name']}} necesita receta médica para que puedas adquirirlo en una farmacia.">
   </head>
   <body>
