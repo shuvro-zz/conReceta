51c6e0a3a4af65d0222c68ad53f2c062bbe2e44c
fvillena <villenafabian@gmail.com>
2019-03-05 11:21:21 -0300

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/{id?}', function ($id = null) {
+    $med = App\Med::find($id);
+    return view('med',compact('id','med'));
+})->name('med');
